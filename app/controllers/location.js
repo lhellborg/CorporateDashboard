06931babe07d33c0e93112349645aea74828eef6
Linda Hellborg <linda.hellborg@gmail.com>
2016-06-28 14:28:50 +0200

    add google map

---
+++ app/controllers/location.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	zoom: 14,
+  	centerLat1: 37.7833,
+  	centerLng1: -122.4167,
+  	employee1: 40,
+  	centerLat2: 37.7893,
+  	centerLng2: -122.4167,
+  	employee2: 30,
+  	centerLat3: 37.7833,
+  	centerLng3: -122.4117,
+  	employee3: 20
+});
